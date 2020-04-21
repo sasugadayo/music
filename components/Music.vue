22c21778af26be7c7c123db6b78c8d8f7a20dccb
sasugadayo <sasugaff@icloud.com>
2020-04-21 22:12:14 +0900

    herokuデプロイ作業

--- components/Music.vue
+++ components/Music.vue
@@ -1,12 +1,22 @@
 <template>
   <main class="main">
-    <button class="musicBtn" @click="musicStart">
-      Music
-    </button>
     <button class="startBtn" @click="appStart" v-if="isStarted == false">
       Start
     </button>
-    <section class="touchZone" v-else></section>
+    <!-- IDを追加 -->
+    <section
+      @mousedown="ringStart"
+      @touchstart="ringStart"
+      @mouseup="ringStop"
+      @touchend="ringStop"
+      @mouseleave="ringStop"
+      @touchleave="ringStop"
+      @mousemove="ring"
+      @touchmove="ring"
+      class="touchZone"
+      id="touchZone"
+      v-else
+    ></section>
   </main>
 </template>
 
@@ -16,20 +26,70 @@ import Tone from "tone";
 export default {
   data() {
     return {
-      isStarted: false
+      isStarted: false,
+      synth: null, // ここを追加
+      isRings: false // ここを追加
     };
   },
   methods: {
     appStart() {
       this.isStarted = true;
-    }, // <- 複数のメソッドを置く場合はコンマが必要
+      this.synth = new Tone.MonoSynth().toMaster(); // ここを追加
+    }, // カンマに注意
+    // --- ここから追加 ---
+    ringStart() {
+      this.isRings = true;
+      this.ring();
+    },
+    ringStop() {
+      this.isRings = false;
+      this.ring();
+    },
+    ring(event) {
+      const minVol = -36;
+      const minFreq = new Tone.Frequency("E1").toFrequency(); // 追加
+      const maxFreq = new Tone.Frequency("E5").toFrequency(); // 追加
+      var x = 0;
+      var y = 0;
 
-    // ====== ここから追加する ======
-    musicStart() {
-      var synth = new Tone.Synth().toMaster();
-      synth.triggerAttackRelease("C4", "8n");
+      if (typeof event !== "undefined") {
+        if (typeof event.touches !== "undefined" && event.touches.length > 0) {
+          x = -event.touches[0].clientX;
+          y = event.touches[0].clientY;
+        } else {
+          x = event.clientX;
+          y = event.clientY;
+        }
+      }
+
+      console.log(x, y);
+
+      // 音量部
+      const height = document.getElementById("touchZone").clientHeight;
+
+      const currentVol = Math.round((y / height) * minVol);
+
+      console.log(currentVol);
+
+      this.synth.volume.value = currentVol;
+
+      // ここから追加
+
+      // 音程部
+      const width = document.getElementById("touchZone").clientWidth;
+      const currentFreq =
+        Math.round((x * (maxFreq - minFreq)) / width) + minFreq;
+
+      console.log(currentFreq);
+
+      // ここまで追加
+
+      if (this.isRings) {
+        this.synth.triggerAttack(currentFreq); // 変更
+      } else {
+        this.synth.triggerRelease();
+      }
     }
-    // ====== ここまで追加する ======
   }
 };
 </script>
@@ -43,8 +103,7 @@ export default {
   align-items: center;
 }
 
-.startBtn,
-.musicBtn {
+.startBtn {
   padding: 10px 20px;
 }
 
