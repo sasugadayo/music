c5f7c6821d556204f48fffe3edde3bc0a17a6b73
sasugadayo <sasugaff@icloud.com>
2020-04-21 20:09:34 +0900

    ボタンクリックすると音が鳴る

---
+++ components/Music.vue
@@ -0,0 +1,60 @@
+<template>
+  <main class="main">
+    <button class="musicBtn" @click="musicStart">
+      Music
+    </button>
+    <button class="startBtn" @click="appStart" v-if="isStarted == false">
+      Start
+    </button>
+    <section class="touchZone" v-else></section>
+  </main>
+</template>
+
+<script>
+import Tone from "tone";
+
+export default {
+  data() {
+    return {
+      isStarted: false
+    };
+  },
+  methods: {
+    appStart() {
+      this.isStarted = true;
+    }, // <- 複数のメソッドを置く場合はコンマが必要
+
+    // ====== ここから追加する ======
+    musicStart() {
+      var synth = new Tone.Synth().toMaster();
+      synth.triggerAttackRelease("C4", "8n");
+    }
+    // ====== ここまで追加する ======
+  }
+};
+</script>
+
+<style>
+.main {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.startBtn,
+.musicBtn {
+  padding: 10px 20px;
+}
+
+/* ここから編集 */
+.touchZone {
+  background: linear-gradient(45deg, rgb(255, 255, 255), rgb(100, 255, 255));
+  width: 100%;
+  min-width: 100%;
+  height: 100vh;
+  min-height: 100vh;
+}
+/* ここまで編集 */
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,15 @@
 <template>
-  <!-- ここから編集 -->
-  <main class="main">
-    <button class="startBtn">Start</button>
-  </main>
-  <!-- ここまで編集 -->
+  <no-ssr>
+    <Music />
+  </no-ssr>
 </template>
 
 <script>
+import Music from "~/components/Music";
+
 export default {
-  // ここから編集
-  data() {
-    return {
-      isStarted: false
-    };
-  },
-  methods: {
-    appStart() {
-      this.isStarted = true;
-    }
+  components: {
+    Music
   }
-  // ここまで編集
 };
 </script>
-
-<style>
-/* ここから編集 */
-.main {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.startBtn {
-  padding: 10px 20px;
-}
-/* ここまで編集 */
-</style>
