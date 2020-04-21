b11a367775fa5169345f14d791c087f87ffe7839
sasugadayo <sasugaff@icloud.com>
2020-04-21 14:22:27 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- ここから編集 -->
+  <main class="main">
+    <button class="startBtn">Start</button>
+  </main>
+  <!-- ここまで編集 -->
+</template>
+
+<script>
+export default {
+  // ここから編集
+  data() {
+    return {
+      isStarted: false
+    };
+  },
+  methods: {
+    appStart() {
+      this.isStarted = true;
+    }
+  }
+  // ここまで編集
+};
+</script>
+
+<style>
+/* ここから編集 */
+.main {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.startBtn {
+  padding: 10px 20px;
+}
+/* ここまで編集 */
+</style>
